d971a2e6b18fe0c7540ed8e93429b622716072f2
Ederlon Barbosa <>
2019-11-18 08:42:43 -0300

    Initial project, created at https://start.spring.io/

---
+++ src/main/java/com/ederlonbarbosa/studiesandtests/StudiesAndTestsApplication.java
@@ -0,0 +1,13 @@
+package com.ederlonbarbosa.studiesandtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiesAndTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiesAndTestsApplication.class, args);
+	}
+
+}
